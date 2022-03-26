ea2a25dee40e5aceced4e82d66c485eb665ead1e
halrang <halrangsae@gmail.com>
2022-03-26 14:35:53 +0900

    9

---
+++ class.js
@@ -0,0 +1,17 @@
+class Person{
+    constructor(name, first, second){
+        this.name = name;
+        this.first = first;
+        this.second = second;
+    }
+}
+ 
+var kim = new Person('kim', 10, 20);
+console.log('kim', kim);
+ 
+// kim.sum = function(){
+//     return 'this : '+(this.first+this.second);
+// }
+// var lee = new Person('lee', 10, 10);
+// console.log("kim.sum()", kim.sum());
+// console.log("lee.sum()", lee.sum());
