7acb3f85f5b271650a6852344984e2e14a9cc06f
halrang <halrangsae@gmail.com>
2022-03-26 15:06:24 +0900

    12

--- class.js
+++ class.js
@@ -9,11 +9,20 @@ class Person{
     }
 }
 class PersonPlus extends Person{
+    constructor(name, first, second, third){
+        super(name, first, second);
+        //부모 class의 생성자
+        this.third = third;
+    }
+    sum(){
+        return super.sum()+this.third;
+        //super.method() : 부모class의 method를 불러옴
+    }
     avg(){
-        return (this.first+this.second)/2;
+        return (this.first+this.second+this.third)/3;
     }
 }
  
-var kim = new PersonPlus('kim', 10, 20);
+var kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
 console.log("kim.avg()", kim.avg());
