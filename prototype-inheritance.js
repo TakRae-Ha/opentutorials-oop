5ceaf06847d772375c11363cdad752c426779aaa
halrang <halrangsae@gmail.com>
2022-03-26 15:21:07 +0900

    13.3

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,10 +1,11 @@
 var superObj = {superVal:'super'}
-var subObj = {subVal:'sub'}
-subObj.__proto__ = superObj;
-//subObj이 superObj의 자식임을 link로 걸어줌
+// var subObj = {subVal:'sub'}
+// subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+//superObj를 부모로하는 subObj생성
+subObj.subVal = 'sub';
+debugger;
 console.log('subObj.subVal =>', subObj.subVal);
 console.log('subObj.superVal =>', subObj.superVal);
-//subObj에는 superVal이 없지만 부모가 가진 value를 호출 함
 subObj.superVal = 'sub';
 console.log('superObj.superVal =>', superObj.superVal);
-//__proto__가 정식적인 javascript의 표준은 아님
