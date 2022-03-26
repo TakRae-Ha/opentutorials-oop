f79aa5c50249c268137c82be339254ba2fc7cb9e
halrang <halrangsae@gmail.com>
2022-03-26 14:02:57 +0900

    6.2

---
+++ object_factory.js
@@ -0,0 +1,25 @@
+var kim = {
+    name:'kim',
+    first:10,
+    second:20,
+    third:30,
+    sum:function(){
+        return this.first+this.second+this.third;
+    }
+}
+var lee = {
+    name:'lee',
+    first:10,
+    second:10,
+    third:10,
+    sum:function(){
+        return this.first+this.second+this.third;
+    }
+}
+console.log("kim.sum()", kim.sum());
+console.log("lee.sum()", lee.sum());
+ 
+var d1 = new Date('2019-4-10');
+console.log('d1.getFullYear()', d1.getFullYear());
+console.log('d1.getMonth()', d1.getMonth());
+//new Date가 d1이라는 객체를 만들어주고 있음//
